8ea4db6f88325f600ae69ccbdcb08f51a38240e1
Test9514 <levayamail123@gmail.com>
2025-06-25 17:51:22 +0300

    Фикстуры Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,31 +1,26 @@
-from playwright.sync_api import sync_playwright, expect
 import pytest
+from playwright.sync_api import expect, Page
+
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
+def test_wrong_email_or_password_authorization(chromium_page: Page):
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
 
-        # email_input = page.locator('//div[@data-testid="login-form-email-input"]//div/input')
-        email_input = page.get_by_test_id('login-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
+    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
+    password_input.fill('password')
 
-        # password_input = page.locator('//div[@data-testid="login-form-password-input"]//div/input')
-        password_input = page.get_by_test_id('login-form-password-input').locator('input')
-        password_input.fill('password')
+    login_button = chromium_page.get_by_test_id('login-page-login-button')
+    login_button.click()
 
-        # login_button = page.locator('//button[@data-testid="login-page-login-button"]')
-        login_button = page.get_by_test_id('login-page-login-button')
-        login_button.click()
+    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
+    expect(wrong_email_or_password_alert).to_be_visible()
 
-        # wrong_email_or_password_alert = page.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
-        wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
-        expect(wrong_email_or_password_alert).to_be_visible()
-        #    expect(wrong_email_or_password_alert).to_be_hidden()
-        expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
 
 #        page.wait_for_timeout(5000)
+
+# Как было изначально можно посмотреть в --> playwright_authorization
